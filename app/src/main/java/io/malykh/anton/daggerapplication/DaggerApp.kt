ce0d3719ca75eec215702c660397ea1167eaf682
Anton Malykh <malykhanton21@gmail.com>
2019-07-02 23:37:58 +0300

    inlined variable

--- app/src/main/java/io/malykh/anton/daggerapplication/DaggerApp.kt
+++ app/src/main/java/io/malykh/anton/daggerapplication/DaggerApp.kt
@@ -2,13 +2,11 @@ package io.malykh.anton.daggerapplication
 
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
-import io.malykh.anton.daggerapplication.di.AppComponent
 import io.malykh.anton.daggerapplication.di.DaggerAppComponent
 
 class DaggerApp: DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        val component: AppComponent = DaggerAppComponent.factory().create(this)
-        return component
+        return DaggerAppComponent.factory().create(this)
     }
 }
